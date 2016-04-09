9ff8cd3d69c31e3a3082c63679bf78e532a22972
Sebastian Gehrmann <s.gehrmann@outlook.com>
2016-04-09 16:53:52 -0400

    HMM training + laplace smoothing

--- HW5.lua
+++ HW5.lua
@@ -11,10 +11,32 @@ cmd:option('-classifier', 'hmm', 'classifier to use')
 
 -- Hyperparameters
 cmd:option('-beta', 1, 'beta for F-Score')
+cmd:option('-laplace', 0, 'added counts for laplace smoothing')
 
 -- ...
 function hmm(inputs, targets, valid_inputs, valid_targets, test)
+	local initial = torch.Tensor(nclasses):fill(0)
+	initial[targets[1]] = 1.0
+	local transition = torch.Tensor(nclasses, nclasses):fill(0)
+	local emission = torch.Tensor(nclasses, nfeatures):fill(opt.laplace)
 
+	--train transition and emission
+	local statecount = torch.Tensor(nclasses, nclasses):fill(0)
+	local emissioncount = torch.Tensor(nclasses, nfeatures):fill(opt.laplace*nclasses)
+	local prevstate = targets[1]
+	for i=1, targets:size(1) do
+		--count transitions
+		transition[prevstate][targets[i]] = transition[prevstate][targets[i]] + 1
+		prevstate = targets[i]
+		--count emissions
+		emission[prevstate][inputs[i]] = emission[prevstate][inputs[i]] + 1
+		--update count matrices (for later division)
+		statecount:narrow(2, prevstate, 1):add(1)
+		emissioncount:narrow(2, inputs[i], 1):add(1)
+	end	
+	transition:cdiv(statecount)
+	emission:cdiv(emissioncount)
+	print(emission:narrow(2, 1, 10))
 end
 
 function memm(inputs, targets, valid_inputs, valid_targets, test)
@@ -28,7 +50,7 @@ end
 function viterbi()
 
 end
-	
+
 
 
 
@@ -52,28 +74,30 @@ function fscore(yhat, y)
 end
 
 function main() 
-   -- Parse input params
-   opt = cmd:parse(arg)
-   local f = hdf5.open(opt.datafile, 'r')
-   nclasses = f:read('nclasses'):all():long()[1]
-   nfeatures = f:read('nfeatures'):all():long()[1]
+	-- Parse input params
+	opt = cmd:parse(arg)
+	local f = hdf5.open(opt.datafile, 'r')
+	nclasses = f:read('nclasses'):all():long()[1]
+	nfeatures = f:read('nfeatures'):all():long()[1]
 
-   train_input = f:read('train_input')
-   train_target = f:read('train_output')
+	train_input = f:read('train_input'):all()
+	train_target = f:read('train_output'):all()
 
-   valid_input = f:read('valid_input')
-   valid_target = f:read('valid_output')
+	valid_input = f:read('valid_input'):all()
+	valid_target = f:read('valid_output'):all()
 
-   test_input = f:read('test_input')
-   embeddings = f:read('embeddings') 
+	test_input = f:read('test_input'):all()
+	embeddings = f:read('embeddings'):all()
 
-   --test the f-score function
-   test_tensor = torch.Tensor(10):fill(2)
-   assert(fscore(test_tensor, test_tensor), 10)
+	--test the f-score function
+	test_tensor = torch.Tensor(10):fill(2)
+	assert(fscore(test_tensor, test_tensor), 10)
 
-   -- Train.
-
-   -- Test.
+	-- Train.
+	if opt.classifier == 'hmm' then
+		hmm(train_input, train_target, valid_input, valid_target, test_input)
+	end
+	-- Test.
 end
 
 main()
